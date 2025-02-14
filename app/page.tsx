1f5f7110c68db201e000a10feb79c93319b846cd
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-15 00:08:04 +0530

    Added clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center  justify-between p-24">
+      <main className="">
         <h1>Lets get started with Assistly AI agent App</h1>
       </main>
   );
