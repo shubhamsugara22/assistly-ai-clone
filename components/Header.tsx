605bca4337c6087b519d69bf983f5e955af11a20
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-18 23:24:59 +0530

    Starting with avatar and header icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Link from 'next/link'
 
 function Header() {
   return (
 	<header>
+		<Link href="/">
+		{/* Avatar */}
+		<div>
+		    <h1>Assistly</h1>
+		    <h2 className='text-sm'>Your Customisable AI Chat Agent</h2>
+		</div>
+		    </Link>
 	  
 	</header>
   )
