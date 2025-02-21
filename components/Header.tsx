f43959142ab02c3b66e63be15987a0c791991535
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-21 22:48:00 +0530

    Add signin in and sign out button in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,26 @@
 import Link from 'next/link'
 import Avatar from './Avatar'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 function Header() {
   return (
-	<header>
-		<Link href="/">
+	<header className='bg-white shadow-sm text-gray-800 flex-justify-between p-5'>
+		<Link href="/" className='flex items-center text-4xl font-thin'>
 		<Avatar
-		 seed='PAPAFAM Support Agent'
-		/>
-		{/* Avatar */}
-		<div>
+		 seed='PAPAFAM Support Agent'/>
+		<div className='space-y-1'>
 		    <h1>Assistly</h1>
 		    <h2 className='text-sm'>Your Customisable AI Chat Agent</h2>
 		</div>
 		    </Link>
+		<div className=''>
+			<SignedIn>
+				<UserButton  showName/>
+			</SignedIn>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+		</div>
 	  
 	</header>
   )
