21586adf755c5a0f277fa8aee0c6e4b5bfc6237a
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-23 18:21:25 +0530

    Add sidebar comp for layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '@/components/Header'
+import Sidebar from '@/components/Sidebar';
 
 function AdminLayout({
 	children,
@@ -8,10 +9,9 @@ function AdminLayout({
   }>) {
   return (
 	<div className='flex flex-col flex-1'>
-		{/* Header */}
 		<Header />
 		<div className='flex flex-col lg:flex-row bg-gray-100'>
-			{/* SideBar */}
+			<Sidebar />
 			<div className='flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto'>
 				{children}
 			</div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Link from "next/link";
 
 function Sidebar() {
   return (
 	<div>
+		<ul>
+			<li>
+				<Link href='/create-chatbot'>Link</Link>
+			</li>
+			<li>
+				<Link href='/view-chatbots'>link</Link>
+			</li>
+			<li>
+				<Link href='/review-sessions'>link</Link>
+			</li>
+		</ul>
 	  
 	</div>
   )
