466dcfd0cd997a2f140417ba6bad0df7a0a904ae
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-24 23:14:29 +0530

    Initiate shadcn and add icons and buttons in sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,23 +6,22 @@ function Header() {
   return (
 	<header className='bg-white shadow-sm text-gray-800 flex-justify-between p-5'>
 		<Link href="/" className='flex items-center text-4xl font-thin'>
-		<Avatar
-		 seed='PAPAFAM Support Agent'/>
-		<div className='space-y-1'>
-		    <h1>Assistly</h1>
-		    <h2 className='text-sm'>Your Customisable AI Chat Agent</h2>
-		</div>
-		    </Link>
-		    <div className="flex items-end ">
-			    <SignedIn>
-				    <UserButton  showName/>
-			    </SignedIn>
-			    <SignedOut>
-				    <SignInButton />
-			    </SignedOut>
+		<Avatar seed='PAPAFAM Support Agent'/>
+		    <div className='space-y-1 pl-3'>
+		        <h1>Assistly</h1>
+		        <h2 className='text-sm'>Your Customisable AI Chat Agent</h2>
+		    </div>
+		</Link>
+		<div className="flex items-end">
+			<SignedIn>
+				<UserButton showName />
+			</SignedIn>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
 		    </div>
 	</header>
-  )
+  );
 }
 
 export default Header
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,16 @@
+import { BotMessageSquare } from "lucide-react";
 import Link from "next/link";
 
 function Sidebar() {
   return (
-	<div className=" bg-white text-white p-5">
+	<div className= " bg-white text-white p-5">
 		<ul className="gap-5 flex lg:flex-col">
 			<li className="flex-1">
 				<Link 
 				href='/create-chatbot'
-				className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">Link</Link>
+				className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">
+				<BotMessageSquare className="h-6 w-6 lg:h-8 lg:w-8"/>	
+					Link</Link>
 			</li>
 			<li className="flex-1">
 				<Link href='/view-chatbots'
