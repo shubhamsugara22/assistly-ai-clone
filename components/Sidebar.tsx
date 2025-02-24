ff80613415be3c6120a8bf52e17c17947dfe476c
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-02-24 22:55:19 +0530

    Adding sidebar link ui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 
 function Sidebar() {
   return (
-	<div>
-		<ul>
-			<li>
-				<Link href='/create-chatbot'>Link</Link>
+	<div className=" bg-white text-white p-5">
+		<ul className="gap-5 flex lg:flex-col">
+			<li className="flex-1">
+				<Link 
+				href='/create-chatbot'
+				className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">Link</Link>
 			</li>
-			<li>
-				<Link href='/view-chatbots'>link</Link>
+			<li className="flex-1">
+				<Link href='/view-chatbots'
+				className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">link</Link>
 			</li>
-			<li>
-				<Link href='/review-sessions'>link</Link>
+			<li className="flex-1">
+				<Link href='/review-sessions'
+				className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">link</Link>
 			</li>
 		</ul>
 	  
