1fdc79b26fb6b68d67e4d40fc53a45ae88bdf9ed
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-03-22 23:30:16 +0530

    add new state characteristic and forms

--- app/(admin)/edit-chatbot/[id]/page.tsx
+++ app/(admin)/edit-chatbot/[id]/page.tsx
@@ -15,6 +15,7 @@ import { GetChatbotByIdResponse, GetChatbotByIdVariables } from "@/types/types";
 
 function EditChatbot( { id } :  { id: string } ) {
 	const [url ,setUrl] = useState<string>("");
+	const [newCharacteristic, SetNewCharacteristic] = useState<string>("");
 	const [chatbotName, setChatbotName] = useState<string>("");
 
 	const {data, loading, error } = 
@@ -24,22 +25,22 @@ function EditChatbot( { id } :  { id: string } ) {
 	);
 	useEffect(() => {
 		if (data) {
-			setChatbotName(data.chatbots.name);
+			setChatbotName(data?.chatbots?.name);
 		}
 	}, [data]);
 
 	useEffect(() => {
-		const url = `${BASE_URL}/chatbot/${id}`;
+		const url = `${BASE_URL}chatbot/${id}`;
 
 		setUrl(url);
 	}, [id]);
   return (
-	<div className="px-10 md:p-10">
+	<div className="px-0 md:p-10">
 		<div className="md:sticky md:top-0 z-50 sm:max-w-sm ml-auto space-y-2 md:border p-5 rounded-b-lg md:rounded-lg bg-[#2991EE]">
 			<h2 className="text-white text-sm font-bold">Link to Chat</h2>
 			<p className="text-sm italic text-white"> 
 				Share this link with your customers to start conversations with your chatbot</p>
-		<div className="flex flex-center space-x-2">
+		<div className="flex items-center space-x-2">
             <Link href={url} className="w-full cursor-pointer hover:opacity-50">
 			<Input value={url} readOnly className="cursor-pointer" />
 			</Link>
@@ -73,13 +74,36 @@ function EditChatbot( { id } :  { id: string } ) {
 				value={chatbotName}
 				onChange={(e) => setChatbotName(e.target.value)}
 				placeholder={chatbotName}
-				className="max-w-lg" 
+				className="w-full border-none bg-transparent text-xl font-bold" 
 				required
 				/>
 				<Button type="submit" disabled={!chatbotName}>
-					Update</Button>
+					Update
+				</Button>
 			</form>
 		</div> 
+		<h2 className="text-xl font-bold mt-10">
+			Heres what your AI knows...
+		</h2>
+		<p>
+			Your chatbot is equiped with the following information to assist you
+			in your conversations with your customers & users
+		</p>
+		<div>
+			<form>
+				<Input
+				 type="text"
+				 placeholder="Example: if you are customer sk for prices, provde pricing pages"
+				 value={newCharacteristic}
+				 onChange={(e) => SetNewCharacteristic(e.target.value)}
+				 />
+				<Button
+				 type="submit"
+				 disabled={!newCharacteristic}>
+					Add
+				</Button>
+			</form>
+		</div>	
 	</section>
 </div>
   )
