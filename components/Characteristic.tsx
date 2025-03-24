e4d78059d8902923f2bfd96697f57814f42e49d4
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-03-24 23:40:18 +0530

    Add handle remove characteristic in edit chatbots

--- components/Characteristic.tsx
+++ components/Characteristic.tsx
@@ -1,4 +1,5 @@
 import { ChatbotCharacteristic } from '@/types/types'
+import { useMutation } from '@apollo/client';
 import {  OctagonX } from 'lucide-react';
 import React from 'react'
 
@@ -7,10 +8,24 @@ function Characteristic({
 } : {
 	characteristic: ChatbotCharacteristic;
  }) {
+
+	const [removeCharacteristic] = useMutation(REMOVE_CHARATERISTIC, {
+		// refetchqueries: ["GetChatbotById"],
+	});
+	
+	const handleRemoveCharacteristic = async() => {
+
+
+	}
   return (
   <li className='relative'>
 	{characteristic.cotent}
-	<OctagonX className="w-6 h-6 text-white fill-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50"/>
+	<OctagonX 
+	className="w-6 h-6 text-white fill-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50"
+	onClick={() => {
+
+	}}
+	/>
   </li>
   );
 }
