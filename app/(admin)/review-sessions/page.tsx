fc4847a7bbca36e7c76ba84ebf2fab09ead33f6f
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-04-10 00:01:49 +0530

    Skeleton page for view session done

--- app/(admin)/review-sessions/page.tsx
+++ app/(admin)/review-sessions/page.tsx
@@ -14,13 +14,23 @@ async function ReviewSessions() {
 	GetUserChatbotsVariables 
   >({
 	query: GET_USER_CHATBOTS,
-	variables: { userId: userId}
+	variables: { userId }
   });
 
-	}
+  const sortedChatbotsByUser: Chatbot[] = chatbotsByUser.map((chatbot) => ({
+	...chatbot,
+	chat_sessions: [...chatbot.chat_sessions].sort(
+		(a, b) =>
+			// sort in ascending order 
+		new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+	)
+}));
 	  return (
 	<div>
-	  ReviewSessions
+	 <h1 className="text-xl lg:text-3xl font-semibold mt-10">
+		Chat sessions </h1>
+	 <h2 className="mb-5">
+		Review all the chat session that chat bots have had with your customers</h2>
 	</div>
   )
 }
