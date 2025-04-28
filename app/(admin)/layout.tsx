12acfc9c6f9c5f193b73b57ee16f21777e24cfec
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-04-28 23:31:15 +0530

    Start working on login page for guest

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '@/components/Header'
 import Sidebar from '@/components/Sidebar';
+import { redirect } from 'next/navigation';
+import  { auth } from "@clerk/nextjs/server";
 
 function AdminLayout({
 	children,
   }: Readonly<{
 	children: React.ReactNode;
   }>) {
+	const { userId } = await auth();
+	if (!userId) {
+		return redirect("/login");
+	}
+
   return (
 	<div className='flex flex-col flex-1'>
 		<Header />
---
+++ app/(guest)/login/page.tsx
@@ -0,0 +1,12 @@
+import Avatar from "@/components/Avatar";
+import { SignIn } from "@clerk/nextjs";
+
+function LoginPage() {
+  return (
+	<div>
+	  LoginPage
+	</div>
+  )
+}
+
+export default LoginPage;
