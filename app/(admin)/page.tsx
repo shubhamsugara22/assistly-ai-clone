0417a58ca3edaa471dea0475498b4a4127e250a1
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-04-29 00:12:09 +0530

    Added style to home page

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <main className="">
-        <h1>Lets get started with Assistly AI agent App</h1>
+      <main className="p-10 bg-white m-10 rounded-md w-full">
+        <h1 className="text-4xl font-light">
+          Welcome to {" "}
+          <span className="text-[#64B5F5] font-semibold">Assistly</span>
+        </h1>
+        <h2 className="mt-2 mb-10">
+          Your Customisable  AI chat agent that helps you manage your customer conversation </h2>
+
+          <Link href="/create-chatbot">
+          <Button className="bg-[#64B5F5]">
+            Lets get  started by creating your first chatbot
+          </Button>
+          </Link>
       </main>
   );
 }
