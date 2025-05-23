945f1450ed92009332cf89b69aed3aeaf481a7c6
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-05-24 00:43:46 +0530

    fetch chatbot char in route api

--- app/api/graphql/send-message/route.ts
+++ app/api/graphql/send-message/route.ts
@@ -1,8 +1,11 @@
-import { NextRequest } from "next/server";
+import { GET_CHATBOT_BY_ID } from "@/graphql/queries/queries";
+import { serverClient } from "@/lib/server/serverClient";
+import { GetChatbotByIdResponse } from "@/types/types";
+import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai =new OpenAI ({
-	apikey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 export async function POST(req: NextRequest) {
 
@@ -11,6 +14,21 @@ export async function POST(req: NextRequest) {
 	console.log(
 		`Received message from chat session ${chat_session_id}: ${content} {chatbot: ${chatbot_id}}`
 	);
-	
+    try {
+		//Step 1: Fetch chatbot charateristics
+		const { data } = await serverClient.query<GetChatbotByIdResponse>({
+			query: GET_CHATBOT_BY_ID,
+			variables: { id: chatbot_id},
+		}); 
+		const chatbot = data.chatbots;
+		if (!chatbot) {
+			return NextResponse.json({ err: "Chatbot not found"}, { status: 404 });
+		}
+
+
+	} catch (error) {
+		console.error("Erro sending message:", error);
+		return NextResponse.json({ error }, { status: 500 });
+	}
 
 }
