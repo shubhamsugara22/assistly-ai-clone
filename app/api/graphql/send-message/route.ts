db7598c0f1d2c17410d8f58c5246c1181c5e23f8
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-05-24 00:38:29 +0530

    Added openai and request vars

--- app/api/graphql/send-message/route.ts
+++ app/api/graphql/send-message/route.ts
@@ -6,4 +6,11 @@ const openai =new OpenAI ({
 });
 export async function POST(req: NextRequest) {
 
+	const { chat_session_id, chatbot_id, content, name } = await req.json();
+
+	console.log(
+		`Received message from chat session ${chat_session_id}: ${content} {chatbot: ${chatbot_id}}`
+	);
+	
+
 }
