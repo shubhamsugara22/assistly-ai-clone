9f8e002863ec24d2499cdc6ff9fbbb93b9d8cfa6
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-05-26 01:09:02 +0530

    Chat completion and systemprompt added

--- app/api/graphql/send-message/route.ts
+++ app/api/graphql/send-message/route.ts
@@ -1,8 +1,9 @@
-import { GET_CHATBOT_BY_ID } from "@/graphql/queries/queries";
+import { GET_CHATBOT_BY_I, GET_MESSAGES_BY_CHAT_SESSION_ID } from "@/graphql/queries/queries";
 import { serverClient } from "@/lib/server/serverClient";
-import { GetChatbotByIdResponse } from "@/types/types";
+import { GetChatbotByIdResponse, MessagesByChatSessionIdResponse } from "@/types/types";
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs"
 
 const openai =new OpenAI ({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -34,6 +35,37 @@ export async function POST(req: NextRequest) {
 	});
 
 	const previousMessages = messageData.chat_session_messages;
+
+	const formattedPreviousMessages: ChatCompletionMessageParam[] =
+	previousMessages.map((message) => ({
+		role: message.sender === "ai" ? "system" : "user",
+		name: message.sender === "ai" ? "system" : name,
+		content: message.content
+	}));
+
+	//Combine charateristics into a system prompt
+	const systemPrompt = chatbot.chat_characteristics
+	.map((c) => c.content)
+	.join(" + ");
+
+	console.log(systemPrompt)
+
+	const messages: ChatCompletionMessageParam[] = [
+		{
+			role: "system",
+			name: "system",
+			content: `Your are a helpful assisstant talking to  ${name}. If a generic question is asked which is not relevant or in the same scope 
+			or domain as the points in mentioned in the key information section, kindly inform the user thetre only  allowed to search for the 
+			specified content, Use Emoji's where possible. Here is some key information that you need to be aware of ,these are elements you may be asked
+			about ${systemPrompt}`,
+		},
+		...formattedPreviousMessages,
+		{
+			role: "user",
+			name: name,
+			content: content,
+		}
+	]
 
 	} catch (error) {
 		console.error("Erro sending message:", error);
